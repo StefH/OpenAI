45dabc71f9a64a4a30d9a06fc3bd829d8e19fb32
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-09-21 14:59:33 +0200

    .

---
+++ examples/CodeSnippets/RedisExample.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace CodeSnippets;
+
+internal class RedisExample
+{
+
+static async Task Main(string[] args)
+{
+    using var host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton<IConnectionMultiplexer>(_ =>
+            {
+                var options = new ConfigurationOptions { EndPoints = { "localhost:6380" } };
+                return ConnectionMultiplexer.Connect(options);
+            });
+        })
+        .Build();
+
+    // Get IConnectionMultiplexer
+    var connection = host.Services.GetRequiredService<IConnectionMultiplexer>();
+
+    // Get an interactive connection to a database inside Redis.
+    var database = connection.GetDatabase();
+
+    // The prefix used to store the hashes
+    string prefix = "The-Developers-Guide-to-Azure";
+
+    // The index from the text-fragment
+    int idx = 214;
+
+    // The text-fragment
+    string text = "Azure Defender enables extended ...";
+
+    // The number of tokens for the text-fragment (calculated using SharpToken)
+    int tokens = 175;
+
+    // The OpenAI Embeddings API is used to generate the vectors
+    float[] embeddings = new float[] { 1.0f };
+
+    // Create a byte array from embeddings
+    byte[] byteArray = MemoryMarshal.Cast<float, byte>(embeddings).ToArray(); 
+
+    // Insert the Hash-entry with the index, text-fragment, number of tokens and the embedding
+    database.HashSet($"{prefix}:{idx}", new HashEntry[]
+    {
+        new(new RedisValue("idx"), idx),
+        new(new RedisValue("text"), text),
+        new(new RedisValue("tokens"), tokens),
+        new(new RedisValue("embedding"), byteArray)
+    });
+
+
+    
+}
+
+}
---
+++ examples/CodeSnippets/pdf-example.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using UglyToad.PdfPig;
+
+namespace CodeSnippets;
+
+internal class PdfExample
+{
+public IReadOnlyList<string> Split(string filePath)
+{
+    var stringBuilder = new StringBuilder();
+
+    using (var document = PdfDocument.Open(filePath))
+    {
+        foreach (var page in document.GetPages().Where(p => !string.IsNullOrWhiteSpace(p.Text)))
+        {
+            stringBuilder.Append(page.Text.Trim());
+        }
+    }
+
+    string[] lines = SplitToLines(stringBuilder);
+
+    return GetTextFragments(lines)
+        .Where(line => !string.IsNullOrWhiteSpace(line))
+        .ToArray();
+}
+
+private string[] GetTextFragments(string[] lines)
+{
+    throw new NotImplementedException();
+}
+
+private string[] SplitToLines(StringBuilder stringBuilder)
+{
+    throw new NotImplementedException();
+}
+}
